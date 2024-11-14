6b580d8397d9e6eed1eaea2d785ce5d978177fd5
Clecio Antao <clecio_antao@carrefour.com>
2024-11-14 15:08:37 -0300

    fix: Adjusts in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "humanitec-aks"  # Defina o prefixo DNS aqui
 
   default_node_pool {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_D2s_v3"
+    os_disk_size_gb = 30
   }
 
   identity {
@@ -20,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "humanitecstorageacc"
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -31,4 +33,20 @@ resource "azurerm_storage_container" "container" {
   name                  = "test-container"
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.container.name
+}
+
+output "container_endpoint" {
+  value = azurerm_storage_account.storage.primary_blob_endpoint
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,37 +1,19 @@
 variable "resource_group" {
-  description = "Nome do Resource Group a ser criado"
-  type        = string
-  default     = "humanitec-rg-infra"
+  type    = string
+  default = "humanitec-rg-infra"
 }
 
 variable "location" {
-  description = "Localização do Resource Group"
-  type        = string
-  default     = "eastus"
+  type    = string
+  default = "eastus"
 }
 
 variable "aks_cluster_name" {
-  description = "Nome do Cluster AKS"
-  type        = string
-  default     = "humanitec-aks-infra"
+  type    = string
+  default = "humanitec-aks-infra"
 }
 
-variable "client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
+variable "storage_account_name" {
+  type    = string
+  default = "humanitecstorageacc"
 }
