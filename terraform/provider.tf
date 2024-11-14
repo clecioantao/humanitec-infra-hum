50449e3325f76d94ad89a2675b3b7634fc8a68c4
Clecio Antao <clecio_antao@carrefour.com>
2024-11-14 16:06:24 -0300

    fix: Adjusts in workflow - correct provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,8 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.74.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
