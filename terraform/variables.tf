bd146c3eba39e3773b6dadbdb4e0e52699a35c35
Clecio Antao <clecio_antao@carrefour.com>
2024-11-14 14:57:39 -0300

    fix: Correct error in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,19 @@
-# Variáveis de Autenticação para Azure
-variable "HUM_CLIENT_ID" {
-  description = "Azure Client ID para o provisionamento via Humanitec"
+variable "client_id" {
+  description = "Azure Client ID"
   type        = string
 }
 
-variable "HUM_CLIENT_SECRET" {
-  description = "Azure Client Secret para o provisionamento via Humanitec"
+variable "client_secret" {
+  description = "Azure Client Secret"
   type        = string
 }
 
-variable "HUM_SUBSCRIPTION_ID" {
-  description = "Azure Subscription ID para o provisionamento via Humanitec"
+variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
 }
 
-variable "HUM_TENANT_ID" {
-  description = "Azure Tenant ID para o provisionamento via Humanitec"
+variable "tenant_id" {
+  description = "Azure Tenant ID"
   type        = string
-}
-
-# Variáveis de Configuração de Recursos
-variable "resource_group" {
-  description = "Nome do Resource Group"
-  type        = string
-  default     = "humanitec-rg-infra"
-}
-
-variable "location" {
-  description = "Localização para provisionar os recursos no Azure"
-  type        = string
-  default     = "eastus"
-}
-
-variable "aks_cluster_name" {
-  description = "Nome do AKS Cluster"
-  type        = string
-  default     = "humanitec-aks-infra"
 }
