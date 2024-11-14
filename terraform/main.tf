ad0e3f814c99907cd441df63e176289531f131ce
Clecio Antao <clecio_antao@carrefour.com>
2024-11-14 16:27:45 -0300

    fix: Removed resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,17 +31,4 @@ resource "azurerm_kubernetes_cluster" "aks" {
   depends_on = [azurerm_container_registry.acr]
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = "test-container"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
-}
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,14 +6,3 @@ output "acr_name" {
   value = azurerm_container_registry.acr.name
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
-}
-
-output "container_name" {
-  value = azurerm_storage_container.container.name
-}
-
-output "container_endpoint" {
-  value = azurerm_storage_account.storage.primary_blob_endpoint
-}
